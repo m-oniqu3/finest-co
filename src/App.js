68de3c7b49f0d83355a3a2f5590bb3c26800e47d
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-31 09:51:26 -0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "./Header.module.css";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <figure className={styled.logoname}>
-          <img src="" alt="" />
+        <figure className={styled.logo}>
+          <img src={logo} alt="logo" />
           <figcaption>techstop</figcaption>
         </figure>
 
-        <ul>
+        <ul className={styled.links}>
           <Link to="/">Home</Link>
           <Link to="/shop">Shop</Link>
         </ul>
+
+        <div className={styled.icons}>
+          <p>w</p>
+          <p>c</p>
+        </div>
       </nav>
     </header>
   );
