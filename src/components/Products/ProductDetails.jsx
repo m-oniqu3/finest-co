34abc778be2b87dd65d8a3260f59b31dd95efe2f
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-31 18:10:28 -0500

    styled productdetails component

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -5,11 +5,11 @@ import laptop from "../../images/laptop.png";
 const ProductDetails = ({ name, content, price }) => {
   return (
     <section className={styled["product-info"]}>
-      <figure className={styled.header}>
+      <figure className={styled["product"]}>
         <img src={laptop} alt={name} />
       </figure>
 
-      <article>
+      <article className={styled["product-content"]}>
         <h1>{name}</h1>
         <p>{content}</p>
         <p>{price}</p>
