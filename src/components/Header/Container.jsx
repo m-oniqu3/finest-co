e8bdc5c562cbdc4b8520540e0516ec012b9b428c
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-31 10:20:57 -0500

    added img and container component to header

---
+++ src/components/Header/Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "./Container.module.css";
+
+const Container = ({ title, content, buttonText }) => {
+  return <section className={styled.container}>Container</section>;
+};
+
+export default Container;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,27 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "./Header.module.css";
 import logo from "../../images/logo.png";
+import Container from "./Container";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <figure className={styled.logo}>
-          <img src={logo} alt="logo" />
-          <figcaption>techstop</figcaption>
-        </figure>
+    <>
+      <header>
+        <nav>
+          <figure className={styled.logo}>
+            <img src={logo} alt="logo" />
+            <figcaption>techstop</figcaption>
+          </figure>
 
-        <ul className={styled.links}>
-          <Link to="/">Home</Link>
-          <Link to="/shop">Shop</Link>
-        </ul>
+          <ul className={styled.links}>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+          </ul>
 
-        <div className={styled.icons}>
-          <p>w</p>
-          <p>c</p>
-        </div>
-      </nav>
-    </header>
+          <div className={styled.icons}>
+            <p>w</p>
+            <p>c</p>
+          </div>
+        </nav>
+
+        <div className={styled.background}></div>
+        <Container
+          title="extra-ordinary service made affordable"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacus tellus egestas nullam et. Mauris tempor cras eget commodo ut nisl. "
+          buttonText="Shop Now"
+        />
+      </header>
+    </>
   );
 };
 
