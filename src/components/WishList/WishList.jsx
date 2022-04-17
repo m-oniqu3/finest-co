dcb85d99bb376630d31a6ee7098657ef4e5dfc82
Monique McIntyre <skaiwilliams85@gmail.com>
2022-04-17 14:31:31 -0500

    Handled removing item from the wishlist

--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import styled from "./WishList.module.css";
 import Product from "../Products/Product";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
 import { ShopIcon, SolidWishListIcon } from "../../icons/Icons";
+import { addToWishList } from "../../store/features/wishlist/wishlistSlice";
 
 const WishList = () => {
+  //dispatch function
+  const dispatch = useDispatch();
+
+  //get the wishlist items from the store
   const { wishListItems } = useSelector((state) => state.wishList);
 
+  /**dispatch the action to add item to wishlist
+   * since the item is already in the list it will be removed when checked by the action
+   */
+  const wishListHandler = (item) => {
+    dispatch(addToWishList(item));
+  };
+
+  //display the items in the wishlist
   const wishList = wishListItems.map((item) => (
     <div className={styled.items} key={item.id}>
       <Product {...item} />
       <div className={styled.wishbtns}>
-        <div className={styled.icon}>
+        {/* icons for adding to cart and removing from wishlist */}
+        <div className={styled.icon} onClick={() => wishListHandler(item)}>
           <SolidWishListIcon />
         </div>
 
@@ -25,7 +38,10 @@ const WishList = () => {
 
   return (
     <section className={styled["wishlist-wrapper"]}>
-      <h4 className={styled.h4}>Your WishList</h4>
+      <h4 className={styled.h4}>
+        Your WishList
+        {wishListItems.length === 0 && <> is empty. </>}
+      </h4>
       <section className={styled.wishlist}>{wishList}</section>
     </section>
   );
