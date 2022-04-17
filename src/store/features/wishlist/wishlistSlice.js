011783d07df797c5201a566acddf61deab634836
Monique McIntyre <skaiwilliams85@gmail.com>
2022-04-16 22:40:42 -0500

    Created wishlist reducers

--- src/store/features/wishlist/wishlistSlice.js
+++ src/store/features/wishlist/wishlistSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  wishList: [],
+  wishListItems: [],
   amountOfItemsInWishList: 0,
 };
 
@@ -11,8 +11,27 @@ const wishlistSlice = createSlice({
   reducers: {
     addToWishList: (state, action) => {
       const itemToAdd = action.payload;
+      const itemToAddExists = state.wishListItems.find(
+        (item) => item.id === itemToAdd.id
+      );
+      state.amountOfItemsInWishList++;
+
+      if (!itemToAddExists) {
+        state.wishListItems.push({ ...itemToAdd });
+      } else {
+        console.log("item already in cart");
+      }
+    },
+
+    removeItemFromWishList: (state, action) => {
+      const wishItem = state.wishListItems.find(
+        (item) => item.id === action.payload.id
+      );
+      state.amountOfItemsInWishList--;
+      state.wishListItems.filter((item) => item.id !== wishItem.id);
     },
   },
 });
 
+export const { addToWishList, removeItemFromWishList } = wishlistSlice.actions;
 export default wishlistSlice.reducer;
