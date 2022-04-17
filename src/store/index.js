a9e81e7b068e0e52c54d94a359f5cc58c38a9505
Monique McIntyre <skaiwilliams85@gmail.com>
2022-04-16 22:45:03 -0500

    Added reducer to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/products/productSlice";
 import cartReducer from "./features/cart/cartSlice";
+import wishlistReducer from "./features/wishlist/wishlistSlice";
 
 const store = configureStore({
-  reducer: { items: productReducer, cart: cartReducer },
+  reducer: {
+    items: productReducer,
+    cart: cartReducer,
+    wishList: wishlistReducer,
+  },
 });
 
 export default store;
